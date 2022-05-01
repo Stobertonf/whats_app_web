eb949bc269e4e70695b6593017d89cce08e2a02d
Stoberton Francisco <49157502+Stobertonf@users.noreply.github.com>
2022-05-01 16:55:34 -0300

    feat: (Login-Page): Creating login area.

--- lib/telas/login.dart
+++ lib/telas/login.dart
@@ -9,11 +9,102 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  TextEditingController _controllerNome =
+      TextEditingController(text: "Stoberton Francisco");
+  TextEditingController _controllerEmail =
+      TextEditingController(text: "stobertof@gmail.com");
+  TextEditingController _controllerSenha =
+      TextEditingController(text: "12345678");
+
   @override
   Widget build(BuildContext context) {
+    double alturaTela = MediaQuery.maybeOf(context).size.height;
+    double larguraTela = MediaQuery.maybeOf(context).size.width;
     return Scaffold(
       body: Container(
         color: PaletaCores.corFundo,
+        height: alturaTela,
+        width: larguraTela,
+        child: Stack(
+          children: [
+            Positioned(
+              child: Container(
+                width: larguraTela,
+                height: alturaTela * 0.5, //Definindo o espaço da Altura.
+                color: PaletaCores.corPrimaria,
+              ),
+            ),
+            Center(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Card(
+                    elevation: 4,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                    child: Container(
+                      padding: const EdgeInsets.all(40),
+                      width: 500,
+                      child: Column(
+                        children: [
+                          //Email
+                          TextField(
+                            controller: _controllerEmail,
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: const InputDecoration(
+                              hintText: "Email",
+                              labelText: "Email",
+                              suffixIcon: Icon(
+                                Icons.email_outlined,
+                              ),
+                            ),
+                          ),
+
+                          //Senha
+                          TextField(
+                            obscureText: true,
+                            controller: _controllerSenha,
+                            keyboardType: TextInputType.text,
+                            decoration: const InputDecoration(
+                              hintText: "Senha",
+                              labelText: "Senha",
+                              suffixIcon: Icon(
+                                Icons.lock_outline,
+                              ),
+                            ),
+                          ),
+
+                          const SizedBox(height: 20),
+
+                          Container(
+                            width: double.infinity, //Para ocupar o espaço total
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                primary: PaletaCores.corPrimaria,
+                              ),
+                              child: const Padding(
+                                padding: EdgeInsets.symmetric(
+                                  vertical: 8,
+                                ),
+                                child: Text(
+                                  "Login",
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
