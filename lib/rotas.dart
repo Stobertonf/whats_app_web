b29338b6d1794c1ff4853d30652f668bb6ff775a
Stoberton Francisco <49157502+Stobertonf@users.noreply.github.com>
2022-05-08 18:39:09 -0300

    feat:(Routes): Configuring the routes

--- lib/rotas.dart
+++ lib/rotas.dart
@@ -1,5 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:whats_app_web/telas/login.dart';
 
 class Rotas {
-  static Route<dynamic> gerarRota(RouteSettings settings) {}
+  static Route<dynamic> gerarRota(RouteSettings settings) {
+    final args = settings.arguments;
+
+    switch (settings.name) {
+      case "/":
+        return MatererialPageRoute(
+          builder: (_) => Login(),
+        );
+
+      case "/login":
+        return MatererialPageRoute(
+          builder: (_) => Login(),
+        );
+
+      case "/home":
+        return MatererialPageRoute(
+          builder: (_) => Login(),
+        );
+    }
+  }
 }
