688bb49d201ed5c5e1ee37bc5cab16f24fbdad26
Stoberton Francisco <49157502+Stobertonf@users.noreply.github.com>
2022-05-14 18:46:27 -0300

    feat:(Home-Mobile): Creating log out route

--- lib/telas/home_mobile.dart
+++ lib/telas/home_mobile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class HomeMobile extends StatefulWidget {
   const HomeMobile({super.key});
@@ -8,11 +9,34 @@ class HomeMobile extends StatefulWidget {
 }
 
 class _HomeMobileState extends State<HomeMobile> {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text(
-        "Allez, Allez, Allez!!!",
+    return DefaultTabController(
+      length: length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("WhatsApp"),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+              ),
+            ),
+            const SizedBox(
+              width: 3.0,
+            ),
+            IconButton(
+              onPressed: () async {
+                await _auth.signOut();
+                Navigator.pushReplacementNamed(context, "/login");
+              },
+              icon: const Icon(Icons.logout),
+            ),
+          ],
+        ),
       ),
     );
   }
