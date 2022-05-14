a5b9dd6f8d34c8448a1c8c5da56b69d49e17f5df
Stoberton Francisco <49157502+Stobertonf@users.noreply.github.com>
2022-05-14 16:31:40 -0300

    feat:(Home-Mobile): Creating Home-Mobile file

---
+++ lib/telas/home_mobile.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeMobile extends StatefulWidget {
+  const HomeMobile({super.key});
+
+  @override
+  State<HomeMobile> createState() => _HomeMobileState();
+}
+
+class _HomeMobileState extends State<HomeMobile> {
+  @override
+  Widget build(BuildContext context) {}
+}
