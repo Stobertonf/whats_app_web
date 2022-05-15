addd835f6f15ec622e764b6e0a1f076e477f4f96
Stoberton Francisco <49157502+Stobertonf@users.noreply.github.com>
2022-05-15 15:40:33 -0300

    fixe:(Themes): Removing Const and add theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:whats_app_web/rotas.dart';
 import 'package:whats_app_web/telas/login.dart';
+import 'package:whats_app_web/uteis/paleta_cores.dart';
+
+final ThemeData temaPadrao = ThemeData(
+  primaryColor: PaletaCores.corPrimaria,
+  accentColor: PaletaCores.corDestaque,
+);
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       title: "WhatsApp Web",
       debugShowCheckedModeBanner: false,
       // home: Login(),
+      theme: temaPadrao,
       initialRoute: "/",
       onGenerateRoute: Rotas.gerarRota,
     ),
