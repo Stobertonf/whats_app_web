659f4b594bd5dbf3b226783ffd08079f4af4a602
Stoberton Francisco <49157502+Stobertonf@users.noreply.github.com>
2022-05-15 18:06:00 -0300

    feat:(List-Contacts): Creating Lista-Contatos file

---
+++ lib/componentes/lista_contatos.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter/material.dart';
+
+class ListaContatos extends StatefulWidget {
+  const ListaContatos({super.key});
+
+  @override
+  State<ListaContatos> createState() => _ListaContatosState();
+}
+
+class _ListaContatosState extends State<ListaContatos> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
