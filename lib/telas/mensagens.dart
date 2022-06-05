eb09133418e6ef67fb3ba73574782247e0caa1f8
stobertonf <stobertonf@gmail.com>
2022-06-05 15:46:21 -0300

    feat:(Messaging-Interface): Creating message file

--- lib/telas/mensagens.dart
+++ lib/telas/mensagens.dart
@@ -1,15 +1,47 @@
+import '../modelos/usuario.dart';
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class Mensagens extends StatefulWidget {
-  const Mensagens({super.key});
+  final Usuario usuarioDestinatario;
+
+  const Mensagens(this.usuarioDestinatario, {Key? key}) : super(key: key);
 
   @override
   State<Mensagens> createState() => _MensagensState();
 }
 
 class _MensagensState extends State<Mensagens> {
+  late Usuario _usuarioDestinario;
+  _recuperarDadosIniciais() {
+    _usuarioDestinario = widget.usuarioDestinatario;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _recuperarDadosIniciais();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            CircleAvatar(
+              radius: 25,
+              backgroundColor: Colors.grey,
+              backgroundImage: CachedNetworkImageProvider(
+                _usuarioDestinario.urlImagem,
+              ),
+            ),
+            const SizedBox(
+              width: 8,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
