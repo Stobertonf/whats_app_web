a493a9e45dd1064eae9c15e4927144de2ca4e094
stobertonf <stobertonf@gmail.com>
2022-06-12 01:10:37 -0300

    feat:(Selecting-Conversation): Configuring Provider Package

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:whats_app_web/rotas.dart';
 import 'package:whats_app_web/telas/login.dart';
 import 'package:whats_app_web/uteis/paleta_cores.dart';
+import 'package:whats_app_web/provider/conversa_provider.dart';
 
 final ThemeData temaPadrao = ThemeData(
   primaryColor: PaletaCores.corPrimaria,
