c0b74aa253ed7bf142489109191f32be478e3460
stobertonf <stobertonf@gmail.com>
2022-06-11 21:16:01 -0300

    feat:(Web-Interface): Creating The Branch

--- lib/telas/home_web.dart
+++ lib/telas/home_web.dart
@@ -1,3 +1,4 @@
+import '../uteis/paleta_cores.dart';
 import 'package:flutter/material.dart';
 
 class HomeWeb extends StatefulWidget {
@@ -10,9 +11,16 @@ class HomeWeb extends StatefulWidget {
 class _HomeWebState extends State<HomeWeb> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text(
-        "Vai Liverpool",
+    return Scaffold(
+      body: Container(
+        color: PaletaCores.corFundo,
+        child: Stack(children: [
+          Positioned(
+            child: Container(
+              color: PaletaCores.corPrimaria,
+            ),
+          ),
+        ]),
       ),
     );
   }
