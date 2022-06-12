c849b07e3defac67d16dc104d19448145a687151
stobertonf <stobertonf@gmail.com>
2022-06-11 21:28:06 -0300

    feat:(Web-Interface): Creating Side Area Conversations

--- lib/telas/home_web.dart
+++ lib/telas/home_web.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import '../uteis/paleta_cores.dart';
 import 'package:flutter/material.dart';
 
@@ -11,17 +13,55 @@ class HomeWeb extends StatefulWidget {
 class _HomeWebState extends State<HomeWeb> {
   @override
   Widget build(BuildContext context) {
+    final largura = MediaQuery.of(context).size.width;
+    final altura = MediaQuery.of(context).size.height;
+
     return Scaffold(
       body: Container(
         color: PaletaCores.corFundo,
-        child: Stack(children: [
-          Positioned(
-            child: Container(
-              color: PaletaCores.corPrimaria,
+        child: Stack(
+          children: [
+            Positioned(
+              child: Container(
+                width: largura,
+                height: altura * 0.2, //Multiplicando por 20% da altura total
+                color: PaletaCores.corPrimaria,
+              ),
             ),
-          ),
-        ]),
+            Positioned(
+              top: 20,
+              left: 20,
+              right: 20,
+              bottom: 20,
+              child: Row(
+                children: const [
+                  Expanded(
+                    flex: 4,
+                    child: AreaLateralConversas(),
+                  ),
+                  Expanded(
+                    flex: 10,
+                    child: AreaLateralConversas(),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class AreaLateralConversas extends StatelessWidget {
+  const AreaLateralConversas({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      height: 300,
+      color: Colors.orange,
     );
   }
 }
